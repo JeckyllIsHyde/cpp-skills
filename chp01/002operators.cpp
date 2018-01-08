5acd766f0f32690d68c4682ebb7e4fa4be6c02d8
Jaime Andres <jacastillol@gmail.com>
2018-01-07 20:27:35 -0500

    test: type handling

--- chp01/002operators.cpp
+++ chp01/002operators.cpp
@@ -56,11 +56,52 @@ int main() {
   {
     // type handling, it must be used #include <typeinfo>
     char c = 'a';
-    std::cout << "typeid(c): " << typeid(c).name() << std::endl;
+    std::cout << "typeid(c): " << typeid(c).name()
+	      << ", sizeof(c): " << sizeof c << std::endl;
+    unsigned short int usi = 1;
+    std::cout << "typeid(usi): " << typeid(usi).name()
+	      << ", sizeof(usi): " << sizeof usi << std::endl;
+    short int si = 1;
+    std::cout << "typeid(si): " << typeid(si).name()
+	      << ", sizeof(si): " << sizeof si << std::endl;
     int i = 1;
-    std::cout << "typeid(i): " << typeid(i).name() << std::endl;
+    std::cout << "typeid(i): " << typeid(i).name()
+	      << ", sizeof(i): " << sizeof i << std::endl;
+    long int li = 1;
+    std::cout << "typeid(li): " << typeid(li).name()
+	      << ", sizeof(li): " << sizeof li << std::endl;
+    long long int lli = 1;
+    std::cout << "typeid(lli): " << typeid(lli).name()
+	      << ", sizeof(lli): " << sizeof lli << std::endl;
     unsigned int ui = 1;
-    std::cout << "typeid(ui): " << typeid(ui).name() << std::endl;
+    std::cout << "typeid(ui): " << typeid(ui).name()
+	      << ", sizeof(ui): " << sizeof ui << std::endl;
+    float f = 1.0f;
+    std::cout << "typeid(f): " << typeid(f).name()
+	      << ", sizeof(f): " << sizeof f << std::endl;
+    double d = 1.0;
+    std::cout << "typeid(d): " << typeid(d).name()
+	      << ", sizeof(d): " << sizeof d << std::endl;
+    long double ld = {1.0};
+    std::cout << "typeid(ld): " << typeid(ld).name()
+	      << ", sizeof(ld): " << sizeof ld << std::endl;
+  }
+  {
+    double x = 2.0, xn = 1.0, exp_x = 1.0;
+    unsigned long fac = 1;
+    for (unsigned long i=1; i<=10; ++i) {
+      xn*=x;
+      fac*=i;
+      exp_x+=xn/fac;
+      std::cout << "e^x is " << exp_x << '\n';
+    }
+  }
+  { // c++11
+    // range-based for-loop 
+    int primes[] = {2,3,5,7,11,13,17,19};
+    for (int i : primes)
+      std::cout << i << " " ;
+    std::cout << std::endl;
   }
   
   return 0;
